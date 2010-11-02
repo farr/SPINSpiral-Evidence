774031c7f50255ed3fd4c9e052d0607b98cd2cdb
Will M. Farr <w-farr@northwestern.edu>
2010-11-02 08:56:55 -0500

    Added -ignore-coord argument.

--- spinspiral_evidence.ml
+++ spinspiral_evidence.ml
@@ -12,6 +12,7 @@ let nmin = ref 4
 let nmax = ref 8192
 let nbootstrap = ref 0
 let downsample = ref 1
+let icoords = ref []
 
 let options = 
   Arg.align
@@ -19,6 +20,8 @@ let options =
       sprintf "pr additional prior factor (default %g)" !prior);
      ("-ignore-coords", Arg.Set_int ignore_coordinates,
       sprintf "n number of coordinates at end of sample to ignore");
+     ("-ignore-coord", Arg.Int (fun i -> icoords := i :: !icoords), 
+      sprintf "i ignore coordinate i (counting from 0)");
      ("-nmin", Arg.Set_int nmin, 
       sprintf "n minimum boxing number (default %d)" !nmin);
      ("-nmax", Arg.Set_int nmax,
@@ -51,9 +54,28 @@ let output_ev samples =
     end in 
     loop !nmin
 
+let trim_coords samples = 
+  Array.map
+    (fun ({Mcmc.value = coords} as samp) -> 
+      let (_, new_coords) = 
+        Array.fold_right 
+          (fun coord (i,nc) -> 
+            if List.mem i !icoords then 
+              (i-1, nc)
+            else
+              (i-1, coord :: nc))
+          coords
+          (Array.length coords - 1, []) in 
+        {samp with Mcmc.value = Array.of_list new_coords})
+    samples          
+
 let _ = 
   Arg.parse options (fun s -> spinspiral_outputs := s :: !spinspiral_outputs) 
     "spinspiral_evidence.{byte,native} OPTIONS ...";
+  if !ignore_coordinates != 0 && !icoords != [] then begin
+    eprintf "Cannot use both -ignore-coordinates and -ignore-coord!\n";
+    exit 1
+  end;
   Randomize.randomize ();
   let samples = 
     List.map 
@@ -62,8 +84,10 @@ let _ =
   let samples = 
     if !ignore_coordinates > 0 then 
       List.map (fun samps -> trim_coordinates !ignore_coordinates samps) samples
+    else if !icoords != [] then 
+      List.map trim_coords samples 
     else
-      samples in 
+      samples in
   let samples = List.fold_left Array.append [| |] samples in
     output_ev samples;
     if !nbootstrap > 0 then 
