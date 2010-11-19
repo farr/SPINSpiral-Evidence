1849d5f09b383aeb5dfe2a7265a51ba0904fc29a
Will M. Farr <w-farr@northwestern.edu>
2010-11-19 08:39:54 -0600

    Correct BigDog priors in inspnest.

--- inspnest_evidence.ml
+++ inspnest_evidence.ml
@@ -17,10 +17,29 @@ let options =
      ("--bootstrap", Arg.Set_int nbootstrap,
       Printf.sprintf "n Number of samples to output, > 1 implies bootstrap (default %d)" !nbootstrap)]
 
+let pi = 3.1415926535897932385
+
+let prior_norm = 
+  let volume = 
+    0.944218 *. (* Integral of Mc^(-5/6) over 1 <= M1,M2 <= 34, M1+M2 < 35 *)
+      0.1 *. (* Time range *)
+      2.0*.pi *. (* Phi0 *)
+      (1.0/.3.0)*.(100.0*.100.0*.100.0) *. (* Distance *)
+      4.0*.pi *. (* RA and Dec *)
+      4.0*.pi (* Orbit orientation---psi and iota *) in 
+    1.0 /. volume
+
+let prior samp = 
+  let mc = samp.(0) and 
+      dist = samp.(4) and 
+      dec = samp.(6) and 
+      inc = samp.(8) in 
+    mc**(-5.0/.6.0) *. dist*.dist *. (cos dec) *. (sin inc) *. prior_norm
+
 let rescale_samples samples = 
   Array.map
-    (fun ({Mcmc.like_prior = {Mcmc.log_likelihood = ll; log_prior = lp}} as samp) -> 
-      {samp with Mcmc.like_prior = {Mcmc.log_likelihood = ll -. !log_l_noise; log_prior = lp}})
+    (fun ({Mcmc.like_prior = {Mcmc.log_likelihood = ll; log_prior = lp}; value = pt} as samp) -> 
+      {samp with Mcmc.like_prior = {Mcmc.log_likelihood = ll -. !log_l_noise; log_prior = log (prior pt)}})
     samples
 
 let _ = 
