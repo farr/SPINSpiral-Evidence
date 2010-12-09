e1e615239d8b54928203f075fec2663453545292
Will M. Farr <w-farr@northwestern.edu>
2010-12-09 08:48:46 -0500

    Adjusted time interval volume for common inputs.

--- inspnest_evidence.ml
+++ inspnest_evidence.ml
@@ -22,7 +22,7 @@ let pi = 3.1415926535897932385
 let prior_norm = 
   let volume = 
     0.247244 *. (* Integral of Mc^(-11/6) over 1 <= M1,M2 <= 34, M1+M2 < 35 *)
-      0.1 *. (* Time range *)
+      0.2 *. (* Time range in common inputs +/-0.1*)
       2.0*.pi *. (* Phi0 *)
       (1.0/.3.0)*.(100.0*.100.0*.100.0) *. (* Distance *)
       4.0*.pi *. (* RA and Dec *)
